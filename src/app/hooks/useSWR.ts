5f96feddd704908c14f4a7b006215504a6772dbe
kappys <alexsbd1@gmail.com>
2023-05-31 00:14:08 +0200

    fix(): build problems

--- src/app/hooks/useSWR.ts
+++ src/app/hooks/useSWR.ts
@@ -2,4 +2,4 @@ import useSwr from 'swr'
 
 export const fetcher = async (url: string, config?: RequestInit) => await fetch(url, config).then(async res => await res.json())
 
-export const useSWR = (url: any, config?: RequestInit) => useSwr(url, async (url) => await fetcher(url, config))
+export const useSWR = (url: any, config?: RequestInit) => useSwr(url, async (url: any) => await fetcher(url, config))
